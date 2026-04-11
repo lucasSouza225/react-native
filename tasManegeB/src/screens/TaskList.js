833b40dbb15ea0e029710eb4306af06f80732ba1
luisgdev303 <luisg.dev.lu@gmail.com>
2026-04-10 22:29:04 -0300

    estilizando o header do site

---
+++ tasManegeB/src/screens/TaskList.js
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Platform, FlatList } from 'react-native'
+
+import todayImage from '../../assets/img/today.jpg'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+const taskDB = [
+    {
+        id: Math.random(),
+        desc: 'Ler o livro Fúria Vermelha',
+        estimateAt: new Date(),
+        doneAt: new Date()
+    },
+    {
+        id: Math.random(),
+        desc: 'Caminhar com o cachorro',
+        estimateAt: new Date(),
+        doneAt: new Date()
+    },
+    {
+        id: Math.random(),
+        desc: 'Assistir a série GOT',
+        estimateAt: new Date(),
+        doneAt: new Date()
+    }
+]
+
+
+
+export default function TaskList() {
+    return (
+        <View style={styles.container}>
+
+            <ImageBackground source={todayImage} style={styles.background}>
+
+                <View style={styles.iconBar}>
+                    <TouchableOpacity>
+                        <Icon
+                            name="eye"
+                            size={20}
+                            color={'white'}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subtitle}>
+                        {new Date().toLocaleDateString('pt-BR', {
+                            weekday: 'long',
+                            day: '2-digit',
+                            month: 'long',
+                            year: 'numeric'
+                        })}
+                    </Text>
+                </View>
+
+            </ImageBackground>
+            <View style={styles.taskList}>
+                <FlatList
+                    data={taskDB}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({ item }) => <Text>{item.desc}</Text>}
+                />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    background: {
+        flex: 3,
+    },
+
+    taskList: {
+        flex: 7,
+    },
+
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: 20,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+
+    title: {
+        color: 'white',
+        fontSize: 48,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+
+    subtitle: {
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 24,
+        marginBottom: 32,
+    }
+})
